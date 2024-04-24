b47e7cd0e8c1b1d61a1ce0453c026ecbdfe22696
kopz9 <gustavoeduardo438@gmail.com>
2024-04-24 19:56:53 -0300

    add topbar

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,30 @@
+export default function TopNav() {
+  return (
+    <nav className="bg-gray-600 text-white ">
+      <div className="mx-auto max-w-7xl  px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-center">
+          <div className="fixed ml-4 flex items-center gap-8 space-x-4 border-b text-xl">
+            <a
+              href="/"
+              className="rounded-lg p-2 uppercase hover:bg-white hover:text-black"
+            >
+              Home
+            </a>
+            <a
+              href="/"
+              className="rounded-lg p-2 uppercase hover:bg-white hover:text-black"
+            >
+              Projetos
+            </a>
+            <a
+              href="/"
+              className="rounded-lg p-2 uppercase hover:bg-white hover:text-black"
+            >
+              Contato
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
