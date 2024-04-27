a84ca7557b75eac22f34887037c97d9f8e8263d9
kopz9 <gustavoeduardo438@gmail.com>
2024-04-26 23:40:48 -0300

    feat: socials component

---
+++ src/app/_components/socials.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Socials() {
+  return (
+    <>
+      <h1 className="mt-8 text-center text-3xl font-semibold">Socials</h1>
+      <div className="flex justify-center gap-12">
+        <div className="mt-14">
+          <Link href="https://github.com/kopz9" target="_blank">
+            <Image
+              src="/github_logo.png"
+              alt="github"
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className="mt-16">
+          <Link
+            href="https://www.linkedin.com/in/gustavo-eduardo09/"
+            target="_blank"
+          >
+            <Image
+              src="/linkedin_icon.webp"
+              alt="github"
+              width={90}
+              height={100}
+            />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Form from "./_components/form";
+import Socials from "./_components/socials";
 
 export default function HomePage() {
   return (
@@ -13,6 +14,7 @@ export default function HomePage() {
           </p>
         </div>
         <Form />
+        <Socials />
       </main>
     </>
   );
